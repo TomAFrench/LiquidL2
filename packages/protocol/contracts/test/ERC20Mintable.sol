c2a2015b09529b0f3857e97f6e2c8e05801b183a
Tom French <tom@tomfren.ch>
2020-10-10 13:58:36 +0100

    fix: remove public modifier from constructor

--- packages/protocol/contracts/test/ERC20Mintable.sol
+++ packages/protocol/contracts/test/ERC20Mintable.sol
@@ -8,7 +8,7 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * ERC20 contract where anybody is able to mint
  */
 contract ERC20Mintable is ERC20 {
-    constructor() public ERC20("Test", "test") {}
+    constructor() ERC20("Test", "test") {}
 
     function mint(address _to, uint256 _value) public returns (bool) {
         _mint(_to, _value);
