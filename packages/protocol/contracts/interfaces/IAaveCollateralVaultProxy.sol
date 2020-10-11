43469a9219f3640bf69a16b666d28b124061dd1f
Tom French <tom@tomfren.ch>
2020-10-12 00:59:55 +0100

    refactor: remove unused functions from IAaveCollateralVaultProxy

--- packages/protocol/contracts/interfaces/IAaveCollateralVaultProxy.sol
+++ packages/protocol/contracts/interfaces/IAaveCollateralVaultProxy.sol
@@ -25,14 +25,6 @@ interface IAaveCollateralVaultProxy {
         uint256 subtractedValue
     ) external;
 
-    function setModel(address vault, uint256 model) external;
-
-    function getBorrow(address vault) external view returns (address);
-
-    function isVaultOwner(address vault, address owner) external view returns (bool);
-
-    function isVault(address vault) external view returns (bool);
-
     function deposit(
         address vault,
         address aToken,
@@ -57,31 +49,5 @@ interface IAaveCollateralVaultProxy {
         uint256 amount
     ) external;
 
-    function getVaults(address owner) external view returns (address[] memory);
-
     function deployVault(address _asset) external returns (address);
-
-    function getVaultAccountData(address _vault)
-        external
-        view
-        returns (
-            uint256 totalLiquidityUSD,
-            uint256 totalCollateralUSD,
-            uint256 totalBorrowsUSD,
-            uint256 totalFeesUSD,
-            uint256 availableBorrowsUSD,
-            uint256 currentLiquidationThreshold,
-            uint256 ltv,
-            uint256 healthFactor
-        );
-
-    function getAaveOracle() external view returns (address);
-
-    function getReservePriceETH(address reserve) external view returns (uint256);
-
-    function getReservePriceUSD(address reserve) external view returns (uint256);
-
-    function getETHPriceUSD() external view returns (uint256);
-
-    function getAave() external view returns (address);
 }
