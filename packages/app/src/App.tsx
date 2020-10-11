0f7b5f5b727e8a5fc78e5fa3377c1f6deb870905
Tom French <tom@tomfren.ch>
2020-10-11 19:21:47 +0100

    feat: remove App.css

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
 
 function App(): ReactElement {
   return (
